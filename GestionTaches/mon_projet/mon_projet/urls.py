174b30eba1743bb8e5d852acda683102bdd9a0dc
Maye Gning <gningmaye1803@gmail.com>
2025-08-30 12:58:56 +0000

    Mise à jour des urls et views

--- GestionTaches/mon_projet/mon_projet/urls.py
+++ GestionTaches/mon_projet/mon_projet/urls.py
@@ -21,5 +21,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_view, name='home'),
+    path('', views.home_view),
 ]
--- GestionTaches/mon_projet/mon_projet/views.py
+++ GestionTaches/mon_projet/mon_projet/views.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponse
-from django.urls import path
-from . import views
+
 
 def home_view(request):
     return HttpResponse("Bonjour Maye")
